9c1e4e29630daaf691c72103fb647682fe41a01e
bencw99 <ben.cohenwang@gmail.com>
2014-08-26 09:24:44 -0700

    CREATED character abstract class

---
+++ src/character/Character.java
@@ -0,0 +1,18 @@
+package character;
+
+import java.awt.*;
+
+import entity.*;
+
+/**
+ * A class representing a character
+ * 
+ * @author Benjamin Cohen-Wang
+ */
+public abstract class Character extends Entity implements Updateable
+{
+	public void draw(Graphics graphics) 
+	{
+		
+	}
+}
