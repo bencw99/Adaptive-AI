ef8ce0ce5d4f817bad04f689cddb10de824d1bea
bencw99 <ben.cohenwang@gmail.com>
2014-08-26 09:24:01 -0700

    CREATED updateable interface

---
+++ src/entity/Updateable.java
@@ -0,0 +1,14 @@
+package entity;
+
+/**
+ * An interface representing an updateable entity 
+ * 
+ * @author Benjamin Cohen-Wang
+ */
+public interface Updateable 
+{
+	/**
+	 * Updates this updateable instance
+	 */
+	public abstract void update();
+}
