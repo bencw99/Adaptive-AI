23771b9e5edc84d8167630bcee4d1b273ec10b36
bencw99 <ben.cohenwang@gmail.com>
2014-08-28 09:51:09 -0700

    CREATED inactive class

---
+++ src/entity/Inactive.java
@@ -0,0 +1,11 @@
+package entity;
+
+/**
+ * A class representing entities in the game that are inactive
+ * 
+ * @author Benjamin Cohen-Wang
+ */
+public abstract class Inactive extends Entity
+{
+
+}
